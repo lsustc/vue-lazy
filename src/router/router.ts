fae0ca49601f19316bd6301a2a751c141ba0d2ee
liushuai3 <liushuai3@myhexin.com>
2021-01-14 10:46:06 +0800

    fix

--- src/router/router.ts
+++ src/router/router.ts
@@ -9,17 +9,17 @@ const router = new Router({
       path: '/view1',
       name: 'view1',
       alias: '/',
-      component: () => import(/* webpackChunkName: "view1" */ './modules/view1/view1.vue')
+      component: () => import(/* webpackChunkName: "view1" */ '../modules/view1/view1.vue')
     },
     {
       path: '/view2',
       name: 'view2',
-      component: () => import(/* webpackChunkName: "view2" */ './modules/view2/view2.vue')
+      component: () => import(/* webpackChunkName: "view2" */ '../modules/view2/view2.vue')
     },
     {
       path: '/view3',
       name: 'view3',
-      component: () => import(/* webpackChunkName: "view3" */ './modules/view3/view3.vue')
+      component: () => import(/* webpackChunkName: "view3" */ '../modules/view3/view3.vue')
     }
   ]
 })
